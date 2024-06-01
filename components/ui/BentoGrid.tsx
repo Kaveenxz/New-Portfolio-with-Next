670db5490530d4852dc330dec848772b6f32f053
Kaveenxz <kaveenhansithx@gmail.com>
2024-06-01 19:12:50 +0530

    Added BackgroundGradientAnimation component to BentoGridItem with id 6

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/utils/cn";
+import { BackgroundGradientAnimation } from "./GradientBg";
 
 export const BentoGrid = ({
   className,
@@ -72,6 +73,11 @@ export const BentoGridItem = ({
                 />
                 )}
             </div>
+            {id === 6 && (
+                <BackgroundGradientAnimation>
+                    <div className="absolute z-50 flex items-center justify-center text-right font-bold"/>
+                </BackgroundGradientAnimation>
+            )}
         </div>
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
