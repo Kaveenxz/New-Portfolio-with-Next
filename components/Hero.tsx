7582f9cdd5fa38c04b72e37b4e25624d9b2655aa
Kaveenxz <kaveenhansithx@gmail.com>
2024-06-01 17:15:24 +0530

    add button component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import MagicButton from "./ui/MagicButton"
 import { Spotlight } from "./ui/Spotlight"
 import { TextGenerateEffect } from "./ui/TextGenerateEffect"
 
@@ -19,9 +20,15 @@ const Hero = () => {
                     <h2 className=" uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Dynamic Web Magic with Next.js</h2>
                     <TextGenerateEffect
                         className="text-center text-[40px] md:text-5xl lg:text-6xl"
-                        words="Transforming Consepts onto seemless Experence"
+                        words="Transforming Consepts into seemless Experence"
                     />
-                    <p className="text-center">Hi, I'm Kaveen, a Next.js Developer based in Croatia</p>
+                    <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hi, I'm Kaveen, a Next.js Developer based in Croatia</p>
+
+                    <a href="#about">
+                        <MagicButton 
+                            title="Show my Work"
+                        />
+                    </a>
                 </div>
             </div>
         </div>
