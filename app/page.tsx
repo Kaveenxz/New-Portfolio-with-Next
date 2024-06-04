07e00151228d3e94e988ce369e52c57c391ac5a4
Kaveenxz <kaveenhansithx@gmail.com>
2024-06-04 11:23:24 +0530

    Added Experience component to Home page and updated id of Clients component from 'projects' to 'testimonials'

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { FaHome } from "react-icons/fa";
 import RecentProjects from '@/components/RecentProjects'
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
@@ -16,6 +17,7 @@ export default function Home() {
         <Grid/>
         <RecentProjects/>
         <Clients/>
+        <Experience/>
       </div>
       
     </main>
