f81118420848bcd640ca8f7f215f960af32e9f69
Kaveenxz <kaveenhansithx@gmail.com>
2024-06-04 11:51:43 +0530

    Added Approch component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import RecentProjects from '@/components/RecentProjects'
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Approch from "@/components/Approch";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
@@ -18,6 +19,7 @@ export default function Home() {
         <RecentProjects/>
         <Clients/>
         <Experience/>
+        <Approch/>
       </div>
       
     </main>
